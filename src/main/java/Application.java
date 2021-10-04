88b99a6a424c77adae2cf8e6887eade18addf2d2
James Heggs <james.heggs@techreturners.com>
2021-10-04 12:11:49 +0100

    Added some more print statements

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -3,5 +3,6 @@ public class Application {
     public static void main (String [] args) {
         System.out.println("Hi there, How are you?");
         System.out.println("Hello team");
+        System.out.println("There we go");
     }
 }
